cbe1ab84312b111f7911c30f91a389efd2d3d94b
Robert <rhieger@nyc.rr.com>
2017-07-16 19:54:50 -0400

    Rough Adaptation of Tutorial Code Complete

--- js/app.js
+++ js/app.js
@@ -1,31 +1,78 @@
 // app.js
 
-// 1: Assess Status of DOM Readiness.
+$(document).ready(function()    {
 
-$(document).ready(function()  {
+    console.log('The DOM is ready.');
 
-  console.log('The DOM is now loaded.');
+    $('#videoSearch').on('click', function(event)    {
 
-  $('#videoSearch').on('click', function(event) {
+        event.preventDefault();
 
-    event.preventDefault();   // Prevent button default behavior.
+        console.log('Button clicked!');
 
-    console.log('Button clicked!');
+        // Prepare YouTube API request:
 
-  })
+        var request = gapi.client.youtube.search.list({
 
-  function init() {
+            part: 'snippet',
+            type: 'video',
+            maxResults: 3,
+            order: 'viewCount',
+            publishedAfter: '2015-01-01T00:00:00Z',
+            q: encodeURIComponent($('#userQuery').val()).replace(/%20/g, '+')
+            //maxResults: 3,
+            //order: 'viewCount',
+            //publishedAfter: '2015-01-01T00:00Z'
+
+        });
+
+        // Execute the request:
+
+        request.execute(function(response)  {
+
+            console.log(response);
+
+            $('#results').html('');
+
+            var results = response.result;
+
+            $.each(results.items, function(index, item)   {
+
+                console.log(item);
+
+                //$('#results').append(item.id.videoId + ' ' +
+                    //item.snippet.title + '<br />');
+
+                $('#searchResults').append('<h2>' + item.snippet.title + '</h2>' +
+                 '<iframe class="video w100" width="640" height="360"' +
+                 'src="//www.youtube.com/embed/' + item.id.videoId + '"' +
+                 'frameborder="0" allowfullscreen>' +        
+                 '</iframe>');
+
+                // $.get('tpl/item.html', function(data)   {
+
+                //     $('#results').append(data);
+
+                // });
+
+            });
+
+        }); // end request.execute()
+
+        $('#search').val('');
+
+    });    // end $('#searchBtn').on()
+
+}); 
+
+function init() {
 
     gapi.client.setApiKey('AIzaSyBb3dDwwcSlFrZUE19WdMrBp2CiZnSSfFY');
 
-    gapi.client.load('youtube', 'v3', function()  {
+    gapi.client.load('youtube', 'v3', function()    {
 
-      // YouTube API is ready.
-
-      console.log('YouTube API is Ready.');
+        // YouTube API is ready.
 
     });
 
-  }   // end function init()
-
-});
+}
