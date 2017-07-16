f5ade999a744e674fcbc7339dad53b4054708868
Robert <rhieger@nyc.rr.com>
2017-07-16 18:52:00 -0400

    Complete Refactoring of Server Request Code Begun

--- js/app.js
+++ js/app.js
@@ -1,44 +1,13 @@
 // app.js
 
-$(document).ready(function()    {
+// 1: Assess Status of DOM Readiness.
 
-  function start() {
+$(document).ready(function()  {
 
-    // 2. Initialize the JavaScript client library.
+  console.log('The DOM is now loaded.');
 
-    gapi.client.init({
+  console.log('Proof:');
 
-      'apiKey': 'AIzaSyA9HOMNKiV3K5ZiGDVZlOTFovYyu8MgHYg',
+  console.log(jQuery);
 
-      // clientId and scope are optional if auth is not required.
-
-      //'clientId': 'YOUR_WEB_CLIENT_ID.apps.googleusercontent.com',
-      //'scope': 'profile',
-
-    }).then(function() {
-
-      // 3. Initialize and make the API request.
-
-      return gapi.client.request({
-
-      'path': 'https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyA9HOMNKiV3K5ZiGDVZlOTFovYyu8MgHYg&part=snippet,contentDetails,statistics,status'
-
-      });
-
-    }).then(function(response) {
-
-      console.log(response.result);
-
-    }, function(reason) {
-
-      console.log('Error: ' + reason.result.error.message);
-
-    });
-
-  };
-  
-  // 1. Load the JavaScript client library.
-
-  gapi.load('client', start);
-
-});     // end $(document).ready()
+});
