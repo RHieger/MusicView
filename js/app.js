c4b431360465773374b3bcb84b8f287ee8f3e904
Robert <rhieger@nyc.rr.com>
2017-07-17 17:44:54 -0400

    Added topicId to Option Data Attributes

--- js/app.js
+++ js/app.js
@@ -17,12 +17,9 @@ $(document).ready(function()    {
             part: 'snippet',
             type: 'video',
             maxResults: 24,
-            order: 'viewCount',
+            order: 'relevance',
             publishedAfter: '2015-01-01T00:00:00Z',
             q: encodeURIComponent($('#userQuery').val()).replace(/%20/g, '+')
-            //maxResults: 3,
-            //order: 'viewCount',
-            //publishedAfter: '2015-01-01T00:00Z'
 
         }); // end request
 
@@ -42,10 +39,6 @@ $(document).ready(function()    {
 
                 // NOTE: Standard width and height for embedded HD YouTube
                 //       videos is 640 x 360.
-
-                // $('div#searchResults').append('<div>' + '<iframe width="480" height="270"' +
-                //  'src="//www.youtube.com/embed/' + item.id.videoId + '"' +
-                //  'frameborder="0" allowfullscreen>' + '</iframe>' + '</div>');
 
                 $('div#searchResults').append('<div>' + '<iframe width="450" height="253"' +
                  'src="//www.youtube.com/embed/' + item.id.videoId + '"' +
