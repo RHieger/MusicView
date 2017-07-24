7c87efbd44be31d1d2732b4761f79369dcbbabc5
Robert <rhieger@nyc.rr.com>
2017-07-24 16:23:13 -0400

    Development of MusicView 1.1 in Progress

--- js/app.js
+++ js/app.js
@@ -1,123 +1,31 @@
 // app.js
 //
-// API Code for MusicView v1.0
+// API Code for MusicView v1.1
 //
 // Robert Hieger
 //
-// July 19, 2017
+// July 31, 2017
 
 // Wrap API code in jQuery  that assures the DOM is ready.
 
 $(document).ready(function()  {
 
+  // Log confirmation to console.
+
   console.log('DOM is ready.');
 
-  // Set the topicId for video search:
+  // Variable for YouTube topicId:
 
-  var topicId;    // topicId for API request
+  var topicId;
 
-  $('#genres').on('change', function() {
+  // Extract topicId from #genres:
 
-    // Extract topicId from #genres:
+  $('#genres').on('change', function()  {
 
     topicId = $(this).val();
 
-    console.log('Topic ID for this Search: ' + topicId);
+    console.log('Topic ID: ' + topicId);
 
   }); // end $('#genres').on()
 
-  /* FUNCTIONS SUPPORTING CREATION OF QUERY TO BE SENT */
-
-  function buildQuery(topicId)   {
-
-    var request = gapi.client.youtube.search.list({
-
-      part: 'snippet',
-      type: 'video',
-      maxResults: 24,
-      order: 'relevance',
-      topicId: topicId,
-      q: encodeURIComponent($('#userQuery').val()).replace(/%20/g, '+')
-
-    });
-
-    return request;
-
-  }   // end buildQuery()
-
-  /* END FUNCTIONS SUPPORTING CREATION OF QUERY TO BE SENT */
-
-  // Send Query to YouTube API:
-
-  $('#videoSearch').on('click', function(event)    {
-
-    event.preventDefault();
-
-    console.log('Button clicked!');
-
-    // Prepare YouTube API request:
-
-    var request = buildQuery(topicId);
-
-    console.log('Request Sent to YouTube API: ' + request);
-
-    // Execute the request:
-
-    request.execute(function(response)  {
-
-      console.log(response);
-
-      $('#content').height('2100');
-
-      $('div#searchResults').html('');
-
-      $('#searchHead').fadeIn(2000);
-
-      var results = response.result;
-
-      $.each(results.items, function(index, item)   {
-
-        console.log(item);
-
-        // NOTE: Standard width and height for embedded HD YouTube
-        //       videos is 640 x 360.
-
-        $('div#searchResults').append('<div>' + '<iframe width="425" height="239"' +
-        'src="//www.youtube.com/embed/' + item.id.videoId + '"' +
-        'frameborder="0" allowfullscreen>' + '</iframe>' + '</div>');
-
-      });   // end $.each(results.items)
-
-      $('#license').show();
-
-      $('#attribution').show();
-
-    }); // end request.execute()
-
-    $('#userQuery').val('');
-
-  });    // end $('#searchBtn').on()
-
-});   // end $(document).ready()
-
-
-// Initialize the YouTube API:
-
-function init() {
-
-  // Set the API Developer Key:
-
-  gapi.client.setApiKey('AIzaSyA9HOMNKiV3K5ZiGDVZlOTFovYyu8MgHYg');
-
-  // Load the YouTube API
-
-  gapi.client.load('youtube', 'v3', function()  {
-
-    // YouTube API is ready. No other functionality has been added
-    // to this callback function, at least as yet.
-
-    console.log('YouTube API ready.');
-
-  }); // end gapi.client.load()
-
-}; // end init()
+}); // end $('#generes')
