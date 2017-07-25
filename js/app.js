b4eb4ea38c9d44584278f5bbee3ea36b2ed4a4a9
Robert <rhieger@nyc.rr.com>
2017-07-25 02:25:58 -0400

    Revert "Merge remote-tracking branch 'MusicView/Development'"
    This reverts commit 1efa22b95063bf0912dc15bc10ae0bffed62301c, reversing
    changes made to 4328c71e1ce9a831bd85af87344f0e2d8b4707e1.

--- js/app.js
+++ js/app.js
@@ -1,144 +1,109 @@
 // app.js
 //
-// API Code for MusicView v1.1
+// API Code for MusicView v1.0
 //
 // Robert Hieger
 //
-// July 25, 2017
+// July 19, 2017
 
 // Wrap API code in jQuery  that assures the DOM is ready.
 
 $(document).ready(function()  {
 
-  // Variable for YouTube topicId:
+  console.log('DOM is ready.');
 
-  var topicId;
+  // Set the topicId for video search:
 
-  // Extract topicId from #genres:
+  var topicId;    // topicId for API request
 
-  $('#genres').on('change', function()  {
+  $('#genres').on('change', function() {
+
+    // Extract topicId from #genres:
 
     topicId = $(this).val();
 
+    console.log('Topic ID for this Search: ' + topicId);
+
   }); // end $('#genres').on()
 
-  /* FUNCTIONS SUPPORTING CREATION OF YouTube QUERY */
+  /* FUNCTIONS SUPPORTING CREATION OF QUERY TO BE SENT */
 
-  function buildQuery(topicId)  {
+  function buildQuery(topicId)   {
 
     var request = gapi.client.youtube.search.list({
 
-      part:       'snippet',
-      type:       'video',
+      part: 'snippet',
+      type: 'video',
       maxResults: 24,
-      order:      'relevance',
-      topicId:    topicId,
-      q:          encodeURIComponent($('#userQuery').val()).replace(/%20/g, '+')
+      order: 'relevance',
+      topicId: topicId,
+      q: encodeURIComponent($('#userQuery').val()).replace(/%20/g, '+')
 
-    }); // end request
+    });
 
     return request;
 
-  } // end buildQuery(topicId)
+  }   // end buildQuery()
 
-  function sendQuery()  {
+  /* END FUNCTIONS SUPPORTING CREATION OF QUERY TO BE SENT */
 
-    console.log('Query has been sent to YouTube.');
+  // Send Query to YouTube API:
+
+  $('#videoSearch').on('click', function(event)    {
+
+    event.preventDefault();
+
+    console.log('Button clicked!');
 
     // Prepare YouTube API request:
 
     var request = buildQuery(topicId);
 
+    console.log('Request Sent to YouTube API: ' + request);
+
     // Execute the request:
 
     request.execute(function(response)  {
 
-      // Resize div#conent to 2100px.
+      console.log(response);
 
       $('#content').height('2100');
 
-      // Empty any pre-existing content from
-      // div#searchResults.
-
-      $('#searchResults').empty();
-
-      // 2-Second Fade in of Search Results Heading:
+      $('div#searchResults').html('');
 
       $('#searchHead').fadeIn(2000);
 
-      // Store search results:
-
       var results = response.result;
 
-      /* Loop over each item in results and create
-         a div in which each embedded video iframe
-         may live.
-      */
+      $.each(results.items, function(index, item)   {
 
-      $.each(results.items, function(index, item) {
+        console.log(item);
 
         // NOTE: Standard width and height for embedded HD YouTube
         //       videos is 640 x 360.
 
-        $('#searchResults').append('<div>' + '<iframe width="425" height ="239"' +
-          'src="//www.youtube.com/embed/' + item.id.videoId + '"' +
-          'border="0" allowfullscreen>' + '</iframe>' + '</div>');
+        $('div#searchResults').append('<div>' + '<iframe width="425" height="239"' +
+        'src="//www.youtube.com/embed/' + item.id.videoId + '"' +
+        'frameborder="0" allowfullscreen>' + '</iframe>' + '</div>');
 
-      }); // end each(results.items)
-
-      // Display Creative Commons License.
+      });   // end $.each(results.items)
 
       $('#license').show();
 
-      // Display attribution for Jeremy Besols.
-
-      $('#attribution').show();
-
     }); // end request.execute()
-
-    // Empty User Query Text.
 
     $('#userQuery').val('');
 
-  } // end sendQuery()
+  });    // end $('#searchBtn').on()
 
-  /* END FUNCTIONS SUPPORTING CREATION OF YouTube QUERY */
+});   // end $(document).ready()
 
-  // Send Query to YouTube API:
-
-  // Event Listener for Enter Key:
-
-  $('#userQuery').on('keyup', function(event)  {
-
-    if (event.which === 13 || event.keyCode === 13) {
-
-      sendQuery();
-
-    } // end if
-
-  });
-
-  // Event Listener for Search Button:
-
-  $('#videoSearch').on('click', function(event) {
-
-    // Prevent default behavior of button:
-
-    event.preventDefault();
-
-    // Send Query to YouTube.
-
-    sendQuery();
-
-  });
-
-}); // end $(document).ready()
 
 // Initialize the YouTube API:
 
 function init() {
 
-  // Specify the API Developer Key:
+  // Set the API Developer Key:
 
   gapi.client.setApiKey('AIzaSyA9HOMNKiV3K5ZiGDVZlOTFovYyu8MgHYg');
 
@@ -146,10 +111,11 @@ function init() {
 
   gapi.client.load('youtube', 'v3', function()  {
 
-    // YouTube API is ready. No other functionality has
-    // been added to this callback function. Any TODO
-    // items may be added here.
+    // YouTube API is ready. No other functionality has been added
+    // to this callback function, at least as yet.
+
+    console.log('YouTube API ready.');
 
   }); // end gapi.client.load()
 
-}   // end init()
+}; // end init()
